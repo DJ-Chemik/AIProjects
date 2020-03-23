196c0fb8d00822e4895d3490436ad45281defd2e
DJ Chemik <dj-chemik@wp.pl>
2020-03-23 21:25:01 +0100

    Finish point 15,16,17,18 from exercise3

--- 3. JavaScript/form_check.js
+++ 3. JavaScript/form_check.js
@@ -1,27 +1,61 @@
 function validate(form){
-    let name = form.elements["f_imie"].value;
-    let surname = form.elements["f_nazwisko"].value;
-    let zipCode = form.elements["f_kod"].value;   
-    let street = form.elements["f_ulica"].value;
-    let town = form.elements["f_miasto"].value;
-    let mail = form.elements["f_email"].value;
+    let name = form.elements["f_imie"];
+    let surname = form.elements["f_nazwisko"];
+    let zipCode = form.elements["f_kod"];   
+    let street = form.elements["f_ulica"];
+    let town = form.elements["f_miasto"];
+    let mail = form.elements["f_email"];
     const texts = [name, surname, zipCode, street, town];
-    checkString(name, "Podaj imie!");
-    checkString(surname, "Podaj nazwisko!");
-    checkEmail(mail);
-    checkString(zipCode, "Podaj kod pocztowy!");
-    checkString(street, "Podaj ulice!");
-    checkString(town, "Podaj miasto!");
+    // checkString(name.value, "Podaj imie!");
+    // checkString(surname.value, "Podaj nazwisko!");
+    // checkEmail(mail.value);
+    // checkString(zipCode.value, "Podaj kod pocztowy!");
+    // checkString(street.value, "Podaj ulice!");
+    // checkString(town.value, "Podaj miasto!"); 
+    checkStringAndFocus(name, "Błąd!");
+    checkStringAndFocus(surname, "Błąd!");
+    checkStringAndFocus(zipCode, "Błąd!");
+    checkStringAndFocus(street, "Błąd!");
+    checkStringAndFocus(town, "Błąd!");
+    checkEmailAndFocus(mail, "Błędny e-mail!")
     
 
 }
+
+function checkStringAndFocus(obj, msg) {
+    let str = obj.value;
+    let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
+    if (isWhiteSpaceOrEmpty(str)) {
+        document.getElementById(errorFieldName).innerHTML = msg;
+        document.getElementById(errorFieldName).focus();
+        //obj.focus();
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function checkEmailAndFocus(obj, msg) {
+    let str = obj.value;
+    let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
+    if (!checkEmail(str)) {
+        document.getElementById(errorFieldName).innerHTML = msg;
+        obj.focus();
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+   
 
 function checkEmail(str) {
     let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
     if (email.test(str))
         return true;
     else {
-        alert("Podaj właściwy e-mail");
+        //alert("Podaj właściwy e-mail");
         return false;
     }
 }
