eeaa1f32c07f05a97b30c8e87491b26fa4bddfc4
DJ Chemik <dj-chemik@wp.pl>
2020-03-23 19:53:42 +0100

    Finish point 8,9,10 from exercise3

--- 3. JavaScript/form_check.js
+++ 3. JavaScript/form_check.js
@@ -1,11 +1,17 @@
 function validate(form){
-    let condition = isEmpty(form.elements["f_imie"].value);
-    if (condition){
+    let text = form.elements["f_imie"].value;
+    let condition1 = isEmpty(text);
+    let condition2 = isWhiteSpaceOrEmpty(text);
+    if (condition1 || condition2){
         alert("Podaj imię!");
         return false;
     }else{
         return true;
     }
+}
+
+function isWhiteSpaceOrEmpty(text){
+    return /^[\s\t\r\n]*$/.test(text);
 }
 
 function isEmpty(text){
