fce4a1eea4effacb636f838591ad2f1696103d18
DJ Chemik <dj-chemik@wp.pl>
2020-04-09 23:36:45 +0200

    Ex 5, point 14,15

--- 5. PHP/index.php
+++ 5. PHP/index.php
@@ -7,6 +7,12 @@
     </head>
     <body>
         <h1> Nasz system </h1>
+        <?php
+            require("funkcje.php");
+            if(isset($_POST["wyloguj"])){
+                $_SESSION['zalogowany'] = 0;
+            }
+        ?>
         <form action="logowanie.php" method="post">
             Login: <input type='text' name='login'> <br>
             Hasło: <input type='password' name='password'><br>
--- 5. PHP/user.php
+++ 5. PHP/user.php
@@ -18,5 +18,31 @@
         Imie i nazwisko użytkownika: <?php echo $_SESSION['zalogowanyImie'] ?>
         <br>
         <a href="index.php">Przejdź do index.php</a>
+        <form action="index.php" method="post">
+            <input type='submit' name='wyloguj' value='wyloguj'>
+        </form>
+        <form action='user.php' method='POST' enctype='multipart/form-data'>
+            <input name="myfile" type="file" name="upload">
+        </form>
+        <?php
+            if(isSet($_POST["upload"])){
+                $currentDir = getcwd();
+                $uploadDir = "/";
+                $fileName = $_FILES['myfile']['name'];
+                $fileSize = $_FILES['myfile']['size'];
+                $fileTmpName = $_FILES['myfile']['tmp_name'];
+                $fileType = $_FILES['myfile']['type'];
+                if($fileName != "" && (
+                    $fileType == 'image/png' || $fileType == 'image/jpeg' ||
+                    $fileType == 'image/PNG' || $fileType == 'image/JPEG')
+                ){
+                    $uploadPath = $currentDir . $uploadDir . $fileName;
+                    if(move_uploaded_file($fileTmpName, $uploadPath)){
+                        echo "Trwa ładowanie zdjęcia na serwer FTP";
+                    }
+                }
+            }
+        ?>
+        <img src="fotka.png" alt="Niespodziankowa fotka">
     </body>
 </html>
