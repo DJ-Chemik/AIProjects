8d463cddf98aca2d4e97bcc84135306fdacab4dd
DJ-Chemik <dj-chemik@wp.pl>
2020-04-30 14:32:56 +0200

    Ex 7, point 2,3,4

---
+++ 7. CakePHP/exampleBooksInsert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `books` (`title`, `author`, `genre`) VALUES
+('Ostatnie Życzenie', 'Andrzej Sapkowski', 'Fantasy'),
+('Marsjanin', 'Andy Weir', 'Science-Fiction'),
+('Potop', 'Henryk Sienkiewicz', "Classic");
