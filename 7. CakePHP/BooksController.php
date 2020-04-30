3fe31f014e096047c939d541ad59fac332f8e9db
DJ-Chemik <dj-chemik@wp.pl>
2020-04-30 15:05:20 +0200

    Ex 7, point 8,9

--- 7. CakePHP/BooksController.php
+++ 7. CakePHP/BooksController.php
@@ -7,5 +7,10 @@
         function index(){
             $this->set('books', $this->Book->find('all'));
         }
+
+        function view($id){
+            $this->Book->id = $id;
+            $this->set('book',$this->Book->read());
+        }
     }
 ?>
